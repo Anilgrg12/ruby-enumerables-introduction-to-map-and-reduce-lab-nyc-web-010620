48e05f1922087f67a747540c9508a3dbbb611cb8
amusing-transpiler-2573 <anil_grg@hotmail.com>
2020-01-03 09:08:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,7 +15,9 @@ end
 def map_to_square(source_array)
   source_array.map {|x| x*x}
 end
-def reduce_to_total(source_array,starting_point)
+def reduce_to_total(source_array,starting_point=0)
+  return source_array.reduce(starting_point){|total, element| total+element}
+end
 
 
 
